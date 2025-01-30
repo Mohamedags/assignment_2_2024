dfeb1d1689dd291da2a3585f9977e23e4113a554
Mohamedags <aguenarousm26@gmail.com>
2025-01-30 09:52:05 +0100

    Exam modifications : Adding a publisher of the robot actual velocity in km/h and a service that returns the number if reached and cancelled goals

--- scripts/Action_client.py
+++ scripts/Action_client.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
 import rospy
 import actionlib
 from nav_msgs.msg import Odometry
 from assignment_2_2024.msg import RobotPoseVelocity
 from assignment_2_2024.msg import PlanningAction, PlanningGoal
 from assignment_2_2024.srv import LastTarget, LastTargetResponse
+from std_srvs.srv import Trigger, TriggerResponse
 
 class ActionClientNode:
     def __init__(self):
@@ -20,15 +20,25 @@ class ActionClientNode:
         # Publisher for robot status
         self.robot_state_pub = rospy.Publisher('/robot_status', RobotPoseVelocity, queue_size=10)
         
+        # Publisher for robot velocity in km/h
+        self.velocity_pub = rospy.Publisher('/robot_velocity_kmh', RobotPoseVelocity, queue_size=10)
+        
         # Subscriber for odometry updates
         rospy.Subscriber('/odom', Odometry, self.process_odometry)
 
         # Service for retrieving the last goal
         self.last_target_service = rospy.Service('/last_goal', LastTarget, self.handle_last_target_request)
+        
+        # Service for retrieving the number of goals reached and cancelled
+        self.goal_status_service = rospy.Service('/goal_status', Trigger, self.handle_goal_status_request)
 
         # Variables to store the last goal coordinates
         self.latest_x = None
         self.latest_y = None
+        
+        # Variables to store the number of goals reached and cancelled
+        self.goals_reached = 0
+        self.goals_cancelled = 0
 
     def send_goal(self, x, y):
         goal = PlanningGoal()
@@ -42,6 +52,7 @@ class ActionClientNode:
     def cancel_goal(self):
         rospy.loginfo("Cancelling the current goal.")
         self.goal_client.cancel_goal()
+        self.goals_cancelled += 1
 
     def process_odometry(self, odometry_data):
         state_msg = RobotPoseVelocity()
@@ -50,10 +61,23 @@ class ActionClientNode:
         state_msg.vel_x = odometry_data.twist.twist.linear.x
         state_msg.vel_z = odometry_data.twist.twist.angular.z
         self.robot_state_pub.publish(state_msg)
+        
+        # Publish velocity in km/h
+        velocity_msg = RobotPoseVelocity()
+        velocity_msg.vel_x = odometry_data.twist.twist.linear.x * 3.6
+        velocity_msg.vel_y = odometry_data.twist.twist.linear.y * 3.6
+        self.velocity_pub.publish(velocity_msg)
 
     def handle_last_target_request(self, request):
         rospy.loginfo(f"Returning last target coordinates: x={self.latest_x}, y={self.latest_y}")
         return LastTargetResponse(target_x=self.latest_x, target_y=self.latest_y)
+
+    def handle_goal_status_request(self, request):
+        rospy.loginfo(f"Goals reached: {self.goals_reached}, Goals cancelled: {self.goals_cancelled}")
+        response = TriggerResponse()
+        response.success = True
+        response.message = f"Goals reached: {self.goals_reached}, Goals cancelled: {self.goals_cancelled}"
+        return response
 
     def goal_loop(self):
         while not rospy.is_shutdown():
@@ -68,6 +92,7 @@ class ActionClientNode:
 
                 if status == actionlib.GoalStatus.SUCCEEDED:
                     rospy.loginfo("Goal achieved successfully.")
+                    self.goals_reached += 1
                 elif status == actionlib.GoalStatus.PREEMPTED:
                     rospy.logwarn("Goal was canceled.")
                 else:
